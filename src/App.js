25110194665033adf7a7aa472354893dc359bc77
mahongri <mahongri1993@gmail.com>
2024-05-08 22:37:38 -0400

    react basic day 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import {useRef} from 'react'
 
 function App() {
+  const [value, setValue] = useState("")
+  const inputRef = useRef(null)
+  const showDom = () => {
+    console.log(inputRef.current)
+    console.dir(inputRef.current)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My First React App</h1>
+      <p>Welcome to my first React app!</p>
+      <input
+      type = "text"
+      value = {value}
+      onChange={(e) => setValue(e.target.value)}/>
+      <div>
+        <input type="text" ref={inputRef}/>
+        <button onClick={showDom}>click me</button>
+      </div>
     </div>
   );
 }
